3c0e797221d82a160472bc773673b0f602027b1d
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:52:57 -0800

    Project Created - MessManagement

---
+++ MessManagement/src/messmanagement/MessManagement.java
@@ -0,0 +1,17 @@
+
+package messmanagement;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class MessManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
